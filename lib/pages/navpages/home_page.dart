9584478b0521896bc00203989b60bbff24cb7ac1
Muden <mudennn@yahoo.com>
2023-02-20 22:38:54 +0800

    homepage v2

--- lib/pages/navpages/home_page.dart
+++ lib/pages/navpages/home_page.dart
@@ -12,7 +12,9 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
+          // Sidebar menu dan Avatar
           Container(
             padding: const EdgeInsets.only(top: 70, left: 20),
             child: Row(
@@ -21,10 +23,36 @@ class _HomePageState extends State<HomePage> {
                   Icons.menu,
                   size: 30,
                   color: Colors.black54,
+                ),
+                Expanded(child: Container()),
+                Container(
+                  margin: const EdgeInsets.only(right: 20),
+                  width: 50,
+                  height: 50,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Colors.grey.withOpacity(0.5),
+                  ),
                 )
               ],
             ),
-          )
+          ),
+          const SizedBox(
+            height: 40,
+          ),
+          // Discover text
+          Container(
+            margin: const EdgeInsets.only(left: 20),
+            child: const Text(
+              "Discover",
+              style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+            ),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          // Tab Bar
+          
         ],
       ),
     );
