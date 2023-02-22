6a4a7e453e6a17e9b34aeaaae67e4bab1645a8af
Muden <mudennn@yahoo.com>
2023-02-22 16:26:07 +0800

    2nd Update Detail page

--- lib/pages/detail_page.dart
+++ lib/pages/detail_page.dart
@@ -16,30 +16,74 @@ class _DetailPageState extends State<DetailPage> {
         height: double.maxFinite,
         child: Stack(
           children: [
+            //Image box
+            Positioned(
+              left: 0,
+              right: 0,
+              child: Container(
+                width: double.maxFinite,
+                height: 350,
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage("image/Mountain1.jpg"),
+                      fit: BoxFit.cover),
+                ),
+              ),
+            ),
             // Menu Icon
             Positioned(
-              left: 20,
+              left: 5,
               top: 50,
               child: Row(
                 children: [
                   IconButton(
                     onPressed: () {},
-                    icon: Icon(Icons.menu),
+                    icon: const Icon(Icons.arrow_back_ios_new_rounded),
+                    color: Colors.white,
                   ),
                   // Image box
-                  Positioned(
-                    left: 0,
-                    right: 0,
-                    child: Container(
-                      width: double.maxFinite,
-                      height: 300,
-                      decoration: BoxDecoration(
-                          image: DecorationImage(
-                        image: AssetImage("image/Mountain1.jpg"),
-                      )),
-                    ),
-                  ),
                 ],
+              ),
+            ),
+            Positioned(
+              top: 320,
+              child: Container(
+                padding: const EdgeInsets.only(
+                  left: 20,
+                  right: 20,
+                  top: 30,
+                ),
+                width: MediaQuery.of(context).size.width,
+                height: 500,
+                decoration: const BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(30),
+                    topRight: Radius.circular(30),
+                  ),
+                ),
+                // Data container
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          "Mount Kinabalu",
+                          style: TextStyle(
+                              fontSize: 23, fontWeight: FontWeight.bold),
+                        ),
+                        Text(
+                          "RM 250",
+                          style: TextStyle(
+                              fontSize: 23,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.green),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
               ),
             ),
           ],
