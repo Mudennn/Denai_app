807c72c6785fb5f2dddb03dd4ff48bbafef4e4a2
Muden <mudennn@yahoo.com>
2023-03-05 11:28:36 +0800

    continue home page

--- lib/pages/home_page/components/body.dart
+++ lib/pages/home_page/components/body.dart
@@ -3,15 +3,33 @@ import 'package:flutter/material.dart';
 import '../../../constant.dart';
 import '../../../size_config.dart';
 
-class Body extends StatelessWidget {
+class Body extends StatefulWidget {
   const Body({super.key});
 
   @override
+  State<Body> createState() => _BodyState();
+}
+
+class _BodyState extends State<Body> with TickerProviderStateMixin {
+  var images = {
+    "Mountain1.jpg": "Hiking", // image file : values for text
+    "Mountain2.jpg": "Kayaking",
+    "Mountain3.jpg": "Camping",
+    "tezos1.jpg": "Snorkling",
+  };
+
+  @override
   Widget build(BuildContext context) {
+    TabController _tabController = TabController(length: 3, vsync: this);
     return SafeArea(
       child: SingleChildScrollView(
         child: Column(
           children: [
+            Container(
+              alignment: Alignment.centerLeft,
+              padding: const EdgeInsets.only(top: 20, left: 20, bottom: 20),
+              child: Text("Denai Hiking", style: TextStyle(fontSize: getProportionateScreenWidth(21), fontWeight: FontWeight.bold),),
+            ),
             Padding(
               padding: EdgeInsets.symmetric(
                 horizontal: getProportionateScreenWidth(20),
@@ -47,13 +65,87 @@ class Body extends StatelessWidget {
                     decoration: BoxDecoration(
                       color: textColor.withOpacity(0.1),
                     ),
-                  )
+                    child: Icon(
+                      Icons.list_outlined,
+                      color: headingColor,
+                    ),
+                  ),
                 ],
+              ),
+            ),
+            SizedBox(height: getProportionateScreenHeight(20),),
+            // Tab Bar ///
+            Container(
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: TabBar(
+                  controller: _tabController,
+                  labelColor: buttonColor,
+                  unselectedLabelColor: textColor,
+                  isScrollable: true,
+                  labelPadding: EdgeInsets.only(left: getProportionateScreenWidth(20), right: getProportionateScreenWidth(20),),
+                  indicatorSize: TabBarIndicatorSize.label,
+                  indicator: CircleTabIndicator(color: buttonColor, radius: 4),
+                  tabs: const [
+                    Tab(
+                        text: "Popular",
+                      ),
+                      Tab(
+                        text: "Mountain",
+                      ),
+                      Tab(
+                        text: "Hill",
+                      ),
+                  ],
+                ),
               ),
             )
           ],
         ),
       ),
     );
+  }
+}
+
+// Untuk indicator punya shape
+class CircleTabIndicator extends Decoration {
+  final Color color;
+  double radius;
+
+  CircleTabIndicator({
+    //constructor
+    required this.color,
+    required this.radius,
+  });
+
+  @override
+  BoxPainter createBoxPainter([VoidCallback? onChanged]) {
+    return _CirclePainter(
+      color: color,
+      radius: radius,
+    );
+  }
+}
+
+class _CirclePainter extends BoxPainter {
+  final Color color;
+  double radius;
+
+  _CirclePainter({
+    //constructor
+    required this.color,
+    required this.radius,
+  });
+
+  @override
+  void paint(Canvas canvas, Offset offset, ImageConfiguration configuration) {
+    Paint _paint = Paint();
+    _paint.color = color;
+    _paint.isAntiAlias = true;
+
+    final Offset circleOffset = Offset(
+        configuration.size!.width / 2 - radius / 2,
+        configuration.size!.height - radius); // untuk position circle
+    canvas.drawCircle(offset + circleOffset, radius, _paint);
   }
 }
