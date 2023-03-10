8ebc2cac25ce562c1c57cc28cabe4593b90d49c7
Muden <mudennn@yahoo.com>
2023-03-10 16:57:51 +0800

    main

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
       theme: theme(),
       initialRoute: WelcomePage.routeName,
       routes: routes,
-      // home: DetailPage()
+      // home: MainPage()
       // home: BlocProvider<AppCubits>(
       //   create:(context) => AppCubits(),
       //   child: AppCubitLogics(),
